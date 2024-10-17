1044e91f30f66a46f44987492419b6ce66d85f0a
itz_anurag <anuraggolu123@gmail.com>
2024-10-17 23:41:54 +0530

    dashboard added

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,143 +1,91 @@
-import React from "react";
-import {
-  Navbar,
-  Collapse,
-  Typography,
-  Button,
-  IconButton,
-  List,
-  ListItem,
-  Menu,
-  MenuHandler,
-  MenuList,
-  MenuItem,
-} from "@material-tailwind/react";
-import {
-  ChevronDownIcon,
-  Bars3Icon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
-import {
-  Bars4Icon,
-  GlobeAmericasIcon,
-  NewspaperIcon,
-  PhoneIcon,
-  RectangleGroupIcon,
-  SquaresPlusIcon,
-  SunIcon,
-  TagIcon,
-  UserGroupIcon,
-} from "@heroicons/react/24/solid";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import logo from '../assets/logo.png';
 
-const navListMenuItems = [
-  { title: "Products", description: "Find the perfect solution for your needs.", icon: SquaresPlusIcon },
-  { title: "About Us", description: "Meet and learn about our dedication", icon: UserGroupIcon },
-  { title: "Blog", description: "Find insightful articles and tips.", icon: Bars4Icon },
-  { title: "Services", description: "Learn how we can help you achieve your goals.", icon: SunIcon },
-  { title: "Support", description: "Reach out to us for assistance or inquiries", icon: GlobeAmericasIcon },
-  { title: "Contact", description: "Get in touch with us", icon: PhoneIcon },
-  { title: "News", description: "Read insightful articles, tips, and expert opinions.", icon: NewspaperIcon },
-  { title: "Special Offers", description: "Explore limited-time deals and bundles", icon: TagIcon },
-];
+const pages = ['Home','Dashboard', 'Post', 'About'];
+const settings = ['Profile', 'Community','Settings', 'Dashboard', 'Logout','Baad me Sochenge'];
 
-function NavListMenu() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const renderItems = navListMenuItems.map(({ icon, title, description }, key) => (
-    <a href="#" key={key}>
-      <MenuItem className="flex items-center gap-3 rounded-lg">
-        <div className="flex items-center justify-center rounded-lg !bg-blue-gray-50 p-2">
-          {React.createElement(icon, { strokeWidth: 2, className: "h-6 text-gray-900 w-6" })}
-        </div>
-        <div>
-          <Typography variant="h6" color="black" className="flex items-center text-sm font-bold">
-            {title}
-          </Typography>
-          <Typography variant="paragraph" className="text-xs !font-medium text-black">
-            {description}
-          </Typography>
-        </div>
-      </MenuItem>
-    </a>
-  ));
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
-    <React.Fragment>
-      <Menu open={isMenuOpen} handler={setIsMenuOpen} offset={{ mainAxis: 20 }} placement="bottom" allowHover={true}>
-        <MenuHandler>
-          <Typography as="div" variant="small" className="font-medium">
-            <ListItem
-              className="flex items-center gap-2 py-2 pr-4 font-medium text-black"
-              selected={isMenuOpen || isMobileMenuOpen}
-              onClick={() => setIsMobileMenuOpen((cur) => !cur)}
+    <AppBar position="static">
+      <Container maxWidth="" className=' bg-black'>
+        <Toolbar disableGutters sx={{ height: '140px', display:'flex',}}>
+          <img src={logo} alt="Logo" style={{ height: '100px', width: 'auto' }} />
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', height:'70px', margin:'100px' },alignItems:'center', }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                className='hover:bg-slate-800'
+                sx={{ my: 2, color: 'white', display: 'block' ,fontSize:'25px', p:'10px', margin:'20px'}}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0, marginRight:'50px'}}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" className='hover:bg-slate-500' sx={{ width: 70, height: 70 }}/>
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
             >
-              Resources
-              <ChevronDownIcon strokeWidth={2.5} className={`hidden h-3 w-3 transition-transform lg:block ${isMenuOpen ? "rotate-180" : ""}`} />
-              <ChevronDownIcon strokeWidth={2.5} className={`block h-3 w-3 transition-transform lg:hidden ${isMobileMenuOpen ? "rotate-180" : ""}`} />
-            </ListItem>
-          </Typography>
-        </MenuHandler>
-        <MenuList className="hidden max-w-screen-xl rounded-xl lg:block">
-          <ul className="grid grid-cols-3 gap-y-2 outline-none outline-0">{renderItems}</ul>
-        </MenuList>
-      </Menu>
-      <div className="block lg:hidden">
-        <Collapse open={isMobileMenuOpen}>{renderItems}</Collapse>
-      </div>
-    </React.Fragment>
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography sx={{ textAlign: 'center', fontSize:'25px' }}>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
-
-function NavList() {
-  return (
-    <List className="mt-4 mb-6 p-0 lg:mt-0 lg:mb-0 lg:flex-row lg:p-1">
-      <Typography as="a" href="#" variant="small" color="black" className="font-medium">
-        <ListItem className="flex items-center gap-2 py-2 pr-4">Home</ListItem>
-      </Typography>
-      <NavListMenu />
-      <Typography as="a" href="#" variant="small" color="black" className="font-medium">
-        <ListItem className="flex items-center gap-2 py-2 pr-4">Contact Us</ListItem>
-      </Typography>
-    </List>
-  );
-}
-
-export function NavbarWithMegaMenu() {
-  const [openNav, setOpenNav] = React.useState(false);
-
-  React.useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 960) setOpenNav(false);
-    };
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  return (
-    <Navbar className="mx-auto max-w-screen-xl px-4 py-2">
-      <div className="flex items-center justify-between text-black">
-        <Typography as="a" href="#" variant="h6" className="mr-4 cursor-pointer py-1.5 lg:ml-2">
-          Material Tailwind
-        </Typography>
-        <div className="hidden lg:block">
-          <NavList />
-        </div>
-        <div className="hidden gap-2 lg:flex">
-          <Button variant="outlined" size="sm" className="bg-gray-300 text-black">Log In</Button>
-          <Button variant="gradient" size="sm" className="bg-black text-white">Sign In</Button>
-        </div>
-        <IconButton variant="text" color="black" className="lg:hidden" onClick={() => setOpenNav(!openNav)}>
-          {openNav ? <XMarkIcon className="h-6 w-6" strokeWidth={2} /> : <Bars3Icon className="h-6 w-6" strokeWidth={2} />}
-        </IconButton>
-      </div>
-      <Collapse open={openNav}>
-        <NavList />
-        <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden">
-          <Button variant="outlined" size="sm" className="bg-gray-300 text-black" fullWidth>Log In</Button>
-          <Button variant="gradient" size="sm" className="bg-black text-white" fullWidth>Sign In</Button>
-        </div>
-      </Collapse>
-    </Navbar>
-  );
-}
+export default ResponsiveAppBar;
--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -1,71 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom"
+import GitHubIcon from '@mui/icons-material/GitHub';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import MailIcon from '@mui/icons-material/Mail';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+// import logo from "../../assets/logo/Long - Logo Transparent (White).png"
 
 const Footer = () => {
-    return (
-        <footer className="bg-blue-100/80 font-sans dark:bg-gray-900">
-            <div className="container px-6 py-12 mx-auto">
-                <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
-                    <div className="sm:col-span-2">
-                        <h1 className="max-w-lg text-xl font-semibold tracking-tight text-gray-800 xl:text-2xl dark:text-white">
-                            Subscribe to our newsletter to get an update.
-                        </h1>
+  return (
+    <div className="bg-[#09051D] pt-7" role="footer">
+      <div className="container mx-auto flex flex-col items-center justify-center">
+        {/* <a href="/">
+          <img  alt="logo with text that says StichHub stitch your way" className="w-48 cursor-pointer" loading="lazy"/>
+        </a> */}
+        <p className="text-white text-center mt-4 text-xl font-serif tracking-wide antialiased font-medium">
+          Choose your style, make your own trend. <br /> Select the one you want to get your clothes stitched from!
+        </p>
+        <div className="flex items-center mt-4">
 
-                        <div className="flex flex-col mx-auto mt-6 space-y-3 md:space-y-0 md:flex-row">
-                            <input
-                                id="email"
-                                type="text"
-                                className="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
-                                placeholder="Email Address"
-                            />
-                            <button className="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80">
-                                Subscribe
-                            </button>
-                        </div>
-                    </div>
+          <a href="mailto:stichhub.office@gmail.com" target="_blank" className="text-white  px-3 hover:text-gray-300 transition-colors duration-300 ease-in-out hover:scale-110" aria-label="Mail us at stichhub.office@gmail.com" title="Mail (External Link)" rel="noopener noreferrer">
+            <MailIcon className="text-white text-2xl  hover:text-[#DB4437]" />
+          </a>
+          <a href="https://github.com/UBA-GCOEN/StichHub" target="_blank" className="text-white px-3  hover:text-gray-300  transition-colors duration-300 ease-in-out hover:scale-110" aria-label="Follow us on Github" title="Github (External Link)" rel="noopener noreferrer" >
+            <GitHubIcon className="text-white text-2xl  hover:text-gray-500" />
+          </a>
+          <a href="https://www.youtube.com/channel/UCjkwYo58eYv6ZGqu4IQiU0w" target="_blank" className="text-white px-3  hover:text-gray-300  transition-colors duration-300 ease-in-out hover:scale-110" aria-label="Follow us on Youtube" title="Youtube (External Link)" rel="noopener noreferrer" >
+            <YouTubeIcon className="text-white text-2xl  hover:text-[#c4302b]" />
+          </a>
+          <a href="https://twitter.com/StichHub_" target="_blank" className="text-white px-3   hover:text-gray-300 transition-colors duration-300 ease-in-out hover:scale-110" aria-label="Follow us on Twitter.office@gmail.com" title="Twitter (External Link)" rel="noopener noreferrer" >
+            <TwitterIcon className="text-white text-2xl  hover:text-sky-400" />
+          </a>
+          <a href="https://www.instagram.com/stichhub_/" target="_blank" className="text-white px-3    hover:text-gray-300  transition-colors duration-300 ease-in-out hover:scale-110" aria-label="Follow us on Instagram" title="Instagram (External Link)" rel="noopener noreferrer" >
 
-                    <div>
-                        <p className="font-semibold text-gray-800 dark:text-white">Quick Link</p>
-                        <div className="flex flex-col items-start mt-5 space-y-2">
-                            <p className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">Home</p>
-                            <p className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">Who We Are</p>
-                            <p className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">Our Philosophy</p>
-                        </div>
-                    </div>
+            <InstagramIcon className="text-white text-2xl  hover:text-[#E4405F]" />
+          </a>
+        </div>
+      </div>
+      {/* <hr */}
+      <p className="text-base bg-[#09051D] p-3 text-center text-white font-serif tracking-wide mt-2">&copy; Mentor Connect {new Date().getFullYear()}</p>
+    </div>
 
-                    <div>
-                        <p className="font-semibold text-gray-800 dark:text-white">Industries</p>
-                        <div className="flex flex-col items-start mt-5 space-y-2">
-                            <p className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">Retail & E-Commerce</p>
-                            <p className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">Information Technology</p>
-                            <p className="text-gray-600 transition-colors duration-300 dark:text-gray-300 dark:hover:text-blue-400 hover:underline hover:cursor-pointer hover:text-blue-500">Finance & Insurance</p>
-                        </div>
-                    </div>
-                </div>
-
-                <hr className="my-6 border-gray-200 md:my-8 dark:border-gray-700 h-2" />
-
-                <div className="sm:flex sm:items-center sm:justify-between">
-                    <div className="flex flex-1 gap-4 hover:cursor-pointer">
-                        <img src="https://www.svgrepo.com/show/303139/google-play-badge-logo.svg" width="130" height="110" alt="Google Play" />
-                        <img src="https://www.svgrepo.com/show/303128/download-on-the-app-store-apple-logo.svg" width="130" height="110" alt="App Store" />
-                    </div>
-
-                    <div className="flex gap-4 hover:cursor-pointer">
-                        <img src="https://www.svgrepo.com/show/303114/facebook-3-logo.svg" width="30" height="30" alt="Facebook" />
-                        <img src="https://www.svgrepo.com/show/303115/twitter-3-logo.svg" width="30" height="30" alt="Twitter" />
-                        <img src="https://www.svgrepo.com/show/303145/instagram-2-1-logo.svg" width="30" height="30" alt="Instagram" />
-                        <img src="https://www.svgrepo.com/show/94698/github.svg" width="30" height="30" alt="GitHub" />
-                        <img src="https://www.svgrepo.com/show/22037/path.svg" width="30" height="30" alt="Path" />
-                        <img src="https://www.svgrepo.com/show/28145/linkedin.svg" width="30" height="30" alt="LinkedIn" />
-                        <img src="https://www.svgrepo.com/show/22048/dribbble.svg" width="30" height="30" alt="Dribbble" />
-                    </div>
-                </div>
-                <p className="font-sans p-8 text-start md:text-center md:text-lg md:p-4">
-                    © 2023 You Company Inc. All rights reserved.
-                </p>
-            </div>
-        </footer>
-    );
+  );
 };
 
 export default Footer;
